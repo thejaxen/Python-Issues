6a73a57f804ee9fcff947bdf37addfb7d680dee9
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2023-08-09 13:00:15 +0300

    Python Learning
    #Defining tuples
    #Writing over a tuple by redefining entire tuple

---
+++ PythonLearning1.3.py
@@ -0,0 +1,17 @@
+#Defining tuples
+
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+print("\n")
+
+for dimension in dimensions:
+    print(dimension)
+
+
+#writing over a tuple by redefining entire tuple
+
+dimensions= (400,50)
+for dimension in dimensions:
+    print(dimension)
