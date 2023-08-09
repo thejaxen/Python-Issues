88be1191059f8623dc3ac749bed33e91ce2ce55d
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2023-08-09 12:57:41 +0300

    Python Learning
    #Copying a List

---
+++ PythonLearning1.2.py
@@ -0,0 +1,13 @@
+#Copying a List
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print(my_foods)
+print(friend_foods)
+
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print(my_foods)
+print(friend_foods)
